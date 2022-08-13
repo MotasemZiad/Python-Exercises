0f15d595f63bf0fcdd2af3b60b3b1889f836d9da
MotasemZiad <moatasem.abunema@gmail.com>
2022-08-13 14:32:21 +0300

    some updates

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+from datetime import datetime
 print("Practice Python")
 
-name = "Hima"
-age = 4
 
-print(f"My name is {name} I'm {age} years old.")
+# Exercise 1:
+# Create a program that asks the user to enter their name and their age.
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+name = input("Enter your name:\n")
+age = int(input(f"Hello {name}, How old are you?:\n"))
+
+year_to_be_hundred = 2022 + (100 - age)
+print(f"On {year_to_be_hundred} you will be 100 years old.")
