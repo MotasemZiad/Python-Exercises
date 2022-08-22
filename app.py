1dc20f33b24259a8b60006d9ffbd3e1986085aaa
MotasemZiad <moatasem.abunema@gmail.com>
2022-08-22 11:35:10 +0300

    exercise #3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+# import os
 # OS module
 
 # name = input("Enter a folder name:\n")
@@ -40,3 +40,27 @@ import os
 #         print(f"{number} is Even")
 # else:
 #     print(f"{number} is Odd")
+
+# Exercise 3:
+# Take a list and write a program that prints out all the elements of the list that are less than 5.
+
+# Extras:
+# Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
+# Write this in one line of Python.
+# Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
+
+# def print_smaller_numbers(list_input, endpoint):
+#     new_list = [i for i in list_input if i < endpoint]
+#     # for i in list_input:
+#     #     if(i < 5):
+#     #         new_list.append(i)
+#     print(new_list)
+
+
+# random_list = [1, 1, 2, 3, 5, 8, 13, 21, 12, 17, 23, 47, 9, 15, 28, 34, 55, 89]
+
+# while True:
+#     great_number = int(input("Enter a number (-1 to exit): \n"))
+#     if(great_number == -1):
+#         break
+#     print_smaller_numbers(random_list, great_number)
