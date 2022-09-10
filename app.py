a4eaa398d036ddd95e69d746d08a1680dadd38fa
MotasemZiad <moatasem.abunema@gmail.com>
2022-09-10 18:15:23 +0300

    some updates

--- app.py
+++ app.py
@@ -323,16 +323,25 @@
 # Then I would see the string:
 # Michele is name My
 
-def reverse_order(text_input):
-    return ' '.join(i for i in text_input.split(" ")[::-1])
-    # words = text_input.split(" ")
-    # return ' '.join([i for i in words[::-1]])
+# def reverse_order(text_input):
+#     return ' '.join(i for i in text_input.split(" ")[::-1])
+#     # words = text_input.split(" ")
+#     # return ' '.join([i for i in words[::-1]])
 
 
-while True:
-    text_input = input(
-        "Write any sentence to display it backwards: (type exit to end)\n")
+# while True:
+#     text_input = input(
+#         "Write any sentence to display it backwards: (type exit to end)\n")
 
-    if(text_input.lower() == 'exit'):
-        break
-    print(reverse_order(text_input))
+#     if(text_input.lower() == 'exit'):
+#         break
+#     print(reverse_order(text_input))
+
+# Exercise 16:
+# write a password generator in Python. Be creative with how you generate passwords.
+# strong passwords have a mix of lowercase letters, uppercase letters, numbers, and symbols.
+# The passwords should be random, generating a new password every time the user asks for a new password.
+# Include your run-time code in a main method.
+
+# Extra:
+# Ask the user how strong they want their password to be. For weak passwords, pick a word or two from a list.
